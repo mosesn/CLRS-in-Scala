117b1976a8ffe61206dc7ca55f6f3e1ffe940ec0
Moses <nnythm@gmail.com>
2012-02-22 22:31:30 -0500

    sort stuff

---
+++ 2/3/MergeSort/build.sbt
@@ -0,0 +1,7 @@
+name := "MergeSort"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.7.1" % "test"
---
+++ 2/3/MergeSort/src/test/scala/Test.scala
@@ -0,0 +1,33 @@
+import org.scalatest.FunSuite
+import scala.util.Random
+import scala.util.Sorting
+
+class Test extends FunSuite{
+  test ("Does this work??") {}
+
+  test("This actually eats sorts?") {
+    val sortee = new Array[Int](40)
+    for (x <- 20 until 0 by -1) {
+      sortee(x * 2 - 1) = x
+      sortee(x * 2 - 2) = x
+    }
+    val tmp = Sorter.sort(sortee, scala.math.Ordering.Int)
+    Sorting.quickSort(sortee)
+    assert(sortee sameElements tmp)
+  }
+
+  test("fuzzing test") {
+    val rand = new Random()
+    for (x <- 0 until 100) {
+      val array = new Array[Int](20)
+      for (x <- 0 until 20) {
+        array(x) = rand.nextInt(100)
+      }
+
+      val arr = array.clone
+      Sorting.quickSort(arr)
+      assert(arr sameElements Sorter.sort(array, scala.math.Ordering.Int))
+    }
+  }
+
+}
